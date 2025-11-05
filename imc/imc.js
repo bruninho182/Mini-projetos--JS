0cea122c82eb33d2a917a3c38a6cef0d1825c1f4
Bruno Patrick Ferreira <120754315+bruninho182@users.noreply.github.com>
2025-11-05 07:54:25 -0300

    imc

---
+++ imc/imc.js
@@ -0,0 +1,20 @@
+const calcular = document.getElementById('calcular');
+
+
+function imc () {   
+const nome = document.getElementById('nome').value;
+const altura = document.getElementById('altura').value;
+const peso = document.getSelection('peso').value; 
+const resultado = document.getSelection('resultado');
+
+//validação
+if (nome !== '' && altura !== '' && peso !== '') {
+    alert ('valor preenchido!')
+}else {
+    resultado.
+}
+
+}
+
+
+calcular.addEventListener('click', imc);
