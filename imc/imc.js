036313922a7c7f1c494c398f74aa3cbf00de68dd
Bruno Patrick Ferreira <120754315+bruninho182@users.noreply.github.com>
2025-11-06 06:55:54 -0300

    Finished!

--- imc/imc.js
+++ imc/imc.js
@@ -4,14 +4,36 @@ const calcular = document.getElementById('calcular');
 function imc () {   
 const nome = document.getElementById('nome').value;
 const altura = document.getElementById('altura').value;
-const peso = document.getSelection('peso').value; 
-const resultado = document.getSelection('resultado');
+const peso = document.getElementById('peso').value; 
+const resultado = document.getElementById('resultado');
 
 //validação
 if (nome !== '' && altura !== '' && peso !== '') {
-    alert ('valor preenchido!')
+    
+
+    // .toFixed usado para arrendondar a soma e o (1) em 1 casa.
+    const valorIMC = (peso / (altura * altura)).toFixed(1); 
+
+    let classificacao = '';
+
+    if (valorIMC < 18.5) {
+        classificacao = 'abaixo do peso.';
+    }else if (valorIMC < 25) {
+        classificacao = 'com peso ideal , Parabéns!';
+    }else if (valorIMC < 30) {
+        classificacao = 'levemente acima do paso.';
+    }else if (valorIMC < 35) {
+        classificacao = 'com obesidade grau I'
+    }else if (valorIMC < 40) {
+        classificacao = 'com obesidade grau II'
+    }else {
+        classificacao = 'com obesidade grau III. Cuidado!'
+    }
+
+resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
 }else {
-    resultado.
+    resultado.textContent = 'Preencha todos os campos!!'
 }
 
 }
